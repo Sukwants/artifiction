44b9df8149e2c52f262091e49979f87a1618b14e
Sukwants <95968907+Sukwants@users.noreply.github.com>
2025-07-29 12:58:36 +0800

    fix: Use local images in TargetFunction cards

--- mona_generate/templates/tf_meta_template.js
+++ mona_generate/templates/tf_meta_template.js
@@ -1,14 +1,16 @@
 // generated file, do not edit
 {% for t in tfs %}
-{% if t.badge_type == "misc" %}
+{% if t.badge_type == "character" %}
+import {{ t.name }}_image from "@image/characters/{{ t.four }}_avatar"
+{% else %}
 import {{ t.name }}_image from "@image/{{ t.badge_path }}"
 {% endif %}
 {% endfor %}
 
-const template = "https://upload-bbs.mihoyo.com/game_record/genshin/character_icon/#.png"
-const newTemplate = "https://act-webstatic.mihoyo.com/hk4e/e20200928calculate/item_icon_u9b0pg/#.png"
-const getImage = name => template.replace("#", name)
-const getIcon = hash => newTemplate.replace("#", hash)
+// {# const template = "https://upload-bbs.mihoyo.com/game_record/genshin/character_icon/#.png"
+// const newTemplate = "https://act-webstatic.mihoyo.com/hk4e/e20200928calculate/item_icon_u9b0pg/#.png"
+// const getImage = name => template.replace("#", name)
+// const getIcon = hash => newTemplate.replace("#", hash) #}
 
 export default {
     {% for t in tfs %}
@@ -22,13 +24,13 @@ export default {
             {% endfor %}
         ],
         "for": "{{ t.four }}",
-        {% if t.badge_type == "character" && t.icon_hash == "" -%}
-        badge: getImage("{{ t.character_icon_name }}"),
-        {%- else if t.badge_type == "character" -%}
-        badge: getIcon("{{ t.icon_hash }}"),
-        {%- else -%}
+        // {# {% if t.badge_type == "character" && t.icon_hash == "" -%}
+        // badge: getImage("{{ t.character_icon_name }}"),
+        // {%- else if t.badge_type == "character" -%}
+        // badge: getIcon("{{ t.icon_hash }}"),
+        // {%- else -%} #}
         badge: {{ t.name }}_image,
-        {%- endif %}
+        // {# {%- endif %} #}
         config: [
             {% for config in t.config %}
             {{ config|e("none") }},
